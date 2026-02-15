596a7e91a7702acf8c48c3a442d265bc8f6de64a
dollytas170-creator <dollytas170@gmail.com>
2026-02-15 13:07:28 +0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
 
+const toggleBtn = document.getElementById("langToggle");
+const arElements = document.querySelectorAll(".ar");
+const enElements = document.querySelectorAll(".en");
+
+toggleBtn.addEventListener("click", () => {
+
+    arElements.forEach(el => el.classList.toggle("hidden"));
+    enElements.forEach(el => el.classList.toggle("hidden"));
+
+    if(toggleBtn.textContent === "EN"){
+        toggleBtn.textContent = "AR";
+        document.documentElement.lang = "en";
+        document.documentElement.dir = "ltr";
+    } else {
+        toggleBtn.textContent = "EN";
+        document.documentElement.lang = "ar";
+        document.documentElement.dir = "rtl";
+    }
+
+});
