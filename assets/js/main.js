c6744af9d92512b8c05172b238967387762bc1d6
dollytas170-creator <dollytas170@gmail.com>
2026-02-15 13:46:27 +0400

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// مثال JS بسيط: رسالة عند إرسال الفورم
-const form = document.querySelector('form');
-if(form){
-    form.addEventListener('submit', function(e){
-        e.preventDefault();
-        alert('تم إرسال رسالتك بنجاح! سنتواصل معك قريبًا.');
-        form.reset();
-    });
-}
